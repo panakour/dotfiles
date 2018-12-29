e7f9d6c331152199c41deabe90e1679845a513de
panakour <panakourweb@gmail.com>
2018-12-29 16:52:02 +0200

    vscode only if not installed

--- src/install/vscode.sh
+++ src/install/vscode.sh
@@ -8,8 +8,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Visual Studio Code\n\n"
 
+if ! package_is_installed "code"; then
 
-execute "sudo add-apt-repository -y 'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main'"
-execute "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF"
-execute "sudo apt update"
-execute "sudo apt -y install code"
+    execute "sudo add-apt-repository -y 'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main'"
+    execute "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF"
+    execute "sudo apt update"
+    execute "sudo apt -y install code"
+
+fi
