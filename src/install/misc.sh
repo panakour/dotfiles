66edb051ad2e6408829e67dabe0ac257bd7acad2
panakour <panakourweb@gmail.com>
2019-01-19 20:17:54 +0200

    vlc via snap instead

--- src/install/misc.sh
+++ src/install/misc.sh
@@ -10,7 +10,7 @@ print_in_purple "\n   Miscellaneous\n\n"
 
 install_package "MySQL Workbench" "mysql-workbench"
 install_package "Transmission" "transmission"
-install_package "VLC" "vlc"
+install_package_via_snap "VLC media player" "vlc"
 install_package "FileZilla" "filezilla"
 install_package_via_snap "PhpStorm" "phpstorm" "--classic"
 install_package_via_snap "GoLand" "goland" "--classic"
