e9b7f3c09933eddc18c3724b146d731cabdadccd
panakour <panakourweb@gmail.com>
2019-02-13 11:33:28 +0200

    update phpstorm settings

--- src/phpstorm/main.sh
+++ src/phpstorm/main.sh
@@ -5,8 +5,16 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+print_in_purple "\n   PhpStorm\n\n"
+
 find "$HOME" -maxdepth 1 -type d -name '.PhpStorm*' | while read line; do
     execute \
     "ln -fs $(pwd)/config/options/* $line/config/options/" \
     "$line/config/options/ → $(pwd)/config/options/*"
+    execute \
+    "ln -fs $(pwd)/config/fileTemplates/includes/* $line/config/fileTemplates/includes/" \
+    "$line/config/fileTemplates/includes/ → $(pwd)/config/fileTemplates/includes/*"
+    execute \
+    "ln -fs $(pwd)/config/codestyles/* $line/config/codestyles/" \
+    "$line/config/codestyles/ → $(pwd)/config/codestyles/*"
 done
