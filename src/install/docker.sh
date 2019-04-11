e69f06363bde1e96030c3ad561ecc7bcabace08f
panakour <panakourweb@gmail.com>
2019-04-11 09:48:06 +0300

    fixes build

--- src/install/docker.sh
+++ src/install/docker.sh
@@ -44,6 +44,6 @@ fi
 
 execute "sudo usermod -aG docker $USER"
 
-execute "sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+execute "sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose \
         && sudo chmod +x /usr/local/bin/docker-compose" \
         "Install docker-compose"
