4b4f30f5419abad9b207a2861753f604627a93da
panakour <panakourweb@gmail.com>
2019-10-22 17:14:09 +0300

    autostart create dir first

--- src/preferences/autostart/main.sh
+++ src/preferences/autostart/main.sh
@@ -7,6 +7,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Autostart\n\n"
 
+mkdir -p $HOME/.config/autostart
+
 find ./apps/ -type f -name '*.desktop' | awk -F/ '{print $NF}' | while read line; do
     execute \
     "ln -fs $(pwd)/apps/$line $HOME/.config/autostart/$line" \
