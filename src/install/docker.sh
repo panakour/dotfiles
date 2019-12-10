bdf9a9545a652b8e93c74ecb95a3d254108479d0
panakour <panakourweb@gmail.com>
2019-12-10 10:46:12 +0200

    docker-compose up to 1.25.0

--- src/install/docker.sh
+++ src/install/docker.sh
@@ -47,7 +47,7 @@ installDocker() {
 }
 
 installDockerCompose() {
-    execute "sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose \
+    execute "sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose \
             && sudo chmod +x /usr/local/bin/docker-compose" \
             "Install docker-compose"
 }
