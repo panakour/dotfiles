a3d5a6e1195dbf600b812b1293715c745a169202
panakour <panakourweb@gmail.com>
2020-03-27 16:39:21 +0200

    remove hardcoded version from docker-compose

--- src/install/docker.sh
+++ src/install/docker.sh
@@ -47,7 +47,7 @@ installDocker() {
 }
 
 installDockerCompose() {
-    execute "sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose \
+    execute "sudo curl -L https://github.com/docker/compose/releases/download/$(curl -s "https://api.github.com/repos/docker/compose/releases/latest" | awk -F '"' '/tag_name/{print $4}')/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose \
             && sudo chmod +x /usr/local/bin/docker-compose" \
             "Install docker-compose"
 }
