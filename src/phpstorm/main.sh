a61ae94846351dfdf1a5c188c18120a244e90755
panakour <panakourweb@gmail.com>
2021-08-11 11:42:24 +0300

    fix phpstorm to work with latest version

--- src/phpstorm/main.sh
+++ src/phpstorm/main.sh
@@ -9,23 +9,22 @@ print_in_purple "\n   PhpStorm\n\n"
 
 declare -a CONFIG_DIRS=(
     "options"
-    "fileTemplates/includes"
     "codestyles"
     "keymaps"
 )
 
-find "$HOME" -maxdepth 1 -type d -name '.PhpStorm*' | while read line; do
+find "$HOME/.config/JetBrains" -maxdepth 1 -type d -name 'PhpStorm*' | while read line; do
 
     for i in "${CONFIG_DIRS[@]}"; do
 
-        phpStormConfigDir="$line/config/${i}"
+        phpStormConfigDir="$line/${i}"
         configDir="$(pwd)/config/$i"
         if [[ ! -e "$phpStormConfigDir" ]]; then
              mkdir -p $phpStormConfigDir
         fi
         execute \
-        "ln -fs $configDir/* $line/config/${i}" \
-        "$line/config/${i} → $configDir/*"
+        "ln -fs $configDir/* $phpStormConfigDir" \
+        "$phpStormConfigDir → $configDir/*"
     done
 
 done
