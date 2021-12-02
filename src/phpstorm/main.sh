402c657d6013c64223bdb99477a55ed6832eac3e
panakour <panakourweb@gmail.com>
2021-12-02 12:13:36 +0200

    fix phpstorm setup for macos

--- src/phpstorm/main.sh
+++ src/phpstorm/main.sh
@@ -13,18 +13,33 @@ declare -a CONFIG_DIRS=(
     "keymaps"
 )
 
-find "$HOME/.config/JetBrains" -maxdepth 1 -type d -name 'PhpStorm*' | while read line; do
-
+linked_files() {
     for i in "${CONFIG_DIRS[@]}"; do
-
-        phpStormConfigDir="$line/${i}"
+        phpStormConfigDir="$1/${i}"
         configDir="$(pwd)/config/$i"
+        if [ "${i}" == "keymaps" ]; then
+            configDir="$(pwd)/config/$i/$(get_os)"
+        fi
         if [[ ! -e "$phpStormConfigDir" ]]; then
              mkdir -p $phpStormConfigDir
         fi
         execute \
-        "ln -fs $configDir/* $phpStormConfigDir" \
+        "ln -fs $configDir/* '$phpStormConfigDir'" \
         "$phpStormConfigDir → $configDir/*"
     done
+}
 
-done
+
+if [ "$(get_os)" == "macos" ]; then
+        find "$HOME/Library/Application Support/JetBrains" -maxdepth 1 -type d -name 'PhpStorm*' | while read line; do
+            linked_files "$line"
+        done
+
+else
+    find "$HOME/.config/JetBrains" -maxdepth 1 -type d -name 'PhpStorm*' | while read line; do
+        linked_files "$line"
+    done
+fi
+
+
+
