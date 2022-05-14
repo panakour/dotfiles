244ce99a4669550132730aee72e6997601284313
panakour <panakourweb@gmail.com>
2022-05-14 12:22:25 +0300

    add kustomize

--- src/os/install/ubuntu/k8s.sh
+++ src/os/install/ubuntu/k8s.sh
@@ -11,5 +11,8 @@ print_in_purple "\n   K8s\n\n"
 
 install_package_via_snap "kubectl" "kubectl" "--classic"
 execute \
+    "go install sigs.k8s.io/kustomize/kustomize@latest" \
+    "kustomize"
+execute \
     "wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash" \
     "k3d"
