6b442360264ee30e78adc2aa3915c854a08f17a0
panakour <panakourweb@gmail.com>
2025-07-02 16:30:52 +0300

    use tmux gruvbox theme

--- modules/shared/tmux/default.nix
+++ modules/shared/tmux/default.nix
@@ -1,16 +1,18 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
   home-manager.users.${config.user} = {
     home.file.".tmux.conf" = {
       source = ./tmux.conf;
     };
     programs.tmux = {
-        enable = true;
-        plugins = with pkgs.tmuxPlugins; [
-            sensible
-            yank
-            vim-tmux-navigator
-        ];
-        extraConfig = builtins.readFile ./tmux.conf;
+      enable = true;
+      plugins = with pkgs.tmuxPlugins; [
+        sensible
+        yank
+        vim-tmux-navigator
+        gruvbox
+      ];
+      extraConfig = builtins.readFile ./tmux.conf;
     };
   };
 }
