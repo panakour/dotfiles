b8d2a7e66daf1d4d2f1e0799b6b9d4669205de62
panakour <panakourweb@gmail.com>
2025-08-25 18:03:39 +0300

    fix homedir to respect both linux and darwin

--- modules/shared/home.nix
+++ modules/shared/home.nix
@@ -1,10 +1,10 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   config.home-manager.users.${config.user}.home = {
     # changelog for this version: https://nix-community.github.io/home-manager/release-notes.xhtml
     stateVersion = "25.05";
     username = config.user;
-    homeDirectory = "/Users/${config.user}";
+    homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${config.user}" else "/home/${config.user}";
   };
 
 }
