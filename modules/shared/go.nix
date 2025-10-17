66cb880fdce172336aed88ce08873864c92dcc5d
panakour <panakourweb@gmail.com>
2025-10-17 16:15:55 +0300

    golang: fix GOPATH

--- modules/shared/go.nix
+++ modules/shared/go.nix
@@ -1,11 +1,12 @@
 { config, pkgs, ... }:
 {
-  home-manager.users.${config.user} = {
-
-    programs.go = {
-      enable = true;
-      package = pkgs.go_1_25;
-      env.GOPATH = "go";
+  home-manager.users.${config.user} =
+    { config, ... }:
+    {
+      programs.go = {
+        enable = true;
+        package = pkgs.go_1_25;
+        env.GOPATH = "${config.home.homeDirectory}/go";
+      };
     };
-  };
 }
