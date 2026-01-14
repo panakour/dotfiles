02953fb1a374054c3003802c688212ae06449d24
panakour <panakourweb@gmail.com>
2026-01-14 09:11:57 +0200

    vim keymap to yank filepath for current buffer

--- modules/shared/neovim/lazyvim/lua/config/keymaps.lua
+++ modules/shared/neovim/lazyvim/lua/config/keymaps.lua
@@ -5,3 +5,13 @@
 vim.keymap.set('n', '<leader>e', function()
     require("oil").open()
 end, { desc = 'Open file explorer (Oil)' })
+
+vim.keymap.set('n', '<leader>yp', function()
+    local path = vim.fn.expand('%:p')
+    if path == '' then
+        vim.notify('No file path for current buffer', vim.log.levels.WARN)
+        return
+    end
+    vim.fn.setreg('+', path)
+    vim.notify('Copied: ' .. path)
+end, { desc = 'Yank absolute file path' })
