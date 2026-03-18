a7966e919ffed3fa4f3374226924e244283a68a2
panakour <panakourweb@gmail.com>
2026-03-18 09:02:08 +0200

    simplify macos system preferences

--- modules/macos/system.nix
+++ modules/macos/system.nix
@@ -4,88 +4,58 @@
     primaryUser = config.user;
 
     defaults = {
-      dock = {
-        autohide = true;
-        autohide-delay = 0.0;
-        tilesize = 42;
-        showhidden = true;
-        show-recents = true;
-        show-process-indicators = true;
-        expose-group-apps = true;
-        persistent-apps = [
-          "/System/Applications/Reminders.app"
-          "/System/Applications/Notes.app"
-          "/System/Applications/Calendar.app"
-          "/Applications/Brave Browser.app"
-          "/Applications/Ghostty.app"
-          "/Applications/PhpStorm.app"
-          "/Applications/Slack.app"
-          "/Applications/Claude.app"
-          "/System/Applications/Mail.app"
-          "/System/Applications/Messages.app"
-        ];
-      };
       NSGlobalDomain = {
-        ApplePressAndHoldEnabled = false;
-        KeyRepeat = 2;
         InitialKeyRepeat = 15;
-        AppleShowScrollBars = "Always";
-        NSWindowResizeTime = 0.1;
+        KeyRepeat = 2;
         NSAutomaticCapitalizationEnabled = false;
         NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticInlinePredictionEnabled = false;
         NSAutomaticPeriodSubstitutionEnabled = false;
         NSAutomaticQuoteSubstitutionEnabled = false;
         NSAutomaticSpellingCorrectionEnabled = false;
         NSDocumentSaveNewDocumentsToCloud = false;
-        _HIHideMenuBar = false;
       };
+
+      dock = {
+        autohide = true;
+        autohide-delay = 0.0;
+      };
+
       finder = {
+        AppleShowAllExtensions = true;
         FXDefaultSearchScope = "SCcf";
-        FXPreferredViewStyle = "Nlsv";
-        _FXShowPosixPathInTitle = true;
         FXEnableExtensionChangeWarning = false;
-        AppleShowAllFiles = true;
-        ShowStatusBar = true;
-        ShowPathbar = true;
+        FXPreferredViewStyle = "Nlsv";
       };
-      CustomUserPreferences = {
-        "com.apple.NetworkBrowser" = {
-          BrowseAllInterfaces = true;
-        };
-        "com.apple.screensaver" = {
-          askForPassword = true;
-          askForPasswordDelay = 0;
-        };
-        "com.apple.trackpad" = {
-          scaling = 2;
-        };
-        "com.apple.mouse" = {
-          scaling = 2.5;
-        };
-        "com.apple.desktopservices" = {
-          DSDontWriteNetworkStores = false;
-        };
-        "com.apple.LaunchServices" = {
-          LSQuarantine = true;
-        };
-        "NSGlobalDomain" = {
-          NSNavPanelExpandedStateForSaveMode = true;
-          WebKitDeveloperExtras = true;
-        };
-        "com.apple.Safari" = {
-          IncludeInternalDebugMenu = true;
-          IncludeDevelopMenu = true;
-          WebKitDeveloperExtrasEnabledPreferenceKey = true;
-          ShowFullURLInSmartSearchField = true;
-          HomePage = "";
-          AutoFillCreditCardData = false;
-          AutoFillFromAddressBook = false;
-          AutoFillMiscellaneousForms = false;
-          AutoFillPasswords = false;
-          "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" = true;
-        };
+
+      LaunchServices = {
+        LSQuarantine = true;
       };
+
+      WindowManager = {
+        GloballyEnabled = false;
+      };
+
+      controlcenter = {
+        BatteryShowPercentage = true;
+      };
+
+      loginwindow = {
+        GuestEnabled = false;
+      };
+
+      menuExtraClock = {
+        Show24Hour = true;
+      };
+
     };
   };
 
+  networking.applicationFirewall = {
+    allowSigned = true;
+    allowSignedApp = true;
+    blockAllIncoming = false;
+    enable = true;
+    enableStealthMode = true;
+  };
 }
