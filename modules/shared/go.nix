9c4f3b49f817c2ab4cd1dbb3e6e5a12775723384
panakour <panakourweb@gmail.com>
2026-04-18 15:10:53 +0300

    chore: bump go to 1.26

--- modules/shared/go.nix
+++ modules/shared/go.nix
@@ -5,7 +5,7 @@
     {
       programs.go = {
         enable = true;
-        package = pkgs.go_1_25;
+        package = pkgs.go_1_26;
         env.GOPATH = "${config.home.homeDirectory}/go";
         env.GOPRIVATE = "gitlab.com/learnworlds/*";
       };
